f321a7e28592112eeaf19e424dc26b9053bebf8e
DanoCZE <admin@lukyn-gaming.eu>
2017-02-10 11:01:26 +0100

    Ověřování věku

--- cyklus.sh
+++ cyklus.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 for (( i=1; i<=5; i++ ))
 do
-echo $i. "kolo"
+	echo $i. "Zadej svůj věk: "
+	read vek
+	echo "Tvůj věk je: "$vel "let."
+
+	if [ $vek -lt 18 ] && [ $vek -ge 10 ]; then
+		echo "Je ti mezi 10 až 17."
+	elif [ $vek -lt 10 ]; then
+		echo "Je ti pod 10"
+	else
+		echo "Je ti 18 a víc"
+fi
 done
