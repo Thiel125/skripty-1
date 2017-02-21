2358d0e3ddacf4c886778bec11df4662ff32b063
DanoCZE <admin@lukyn-gaming.eu>
2017-02-21 08:38:57 +0100

    Stránky

--- ip.sh
+++ ip.sh
@@ -6,12 +6,12 @@ down=`echo $down | awk -F ")" '{print $1}'`
 up=`ifconfig | grep "Přijato" | awk -F "(" '{print $3}'`
 up=`echo $up | awk -F ")" '{print $1}'`
 disk=`df -h | grep /dev/sda1 | awk -F " " '{print $4}'`
+users=`who | wc -l`
 cas=`date +"%d.%m.%Y - %H:%M:%S"`
-echo "Moje ip je: "$ip
-echo
-echo "Přijato: "$down
-echo "Odesláno: "$up
-echo
-echo "Volné místo na disku: "$disk"B"
-echo
-echo "Datum: "$cas
+echo "<h1>Moje ip je: "$ip"</h1>" >> /home/student/Plocha/skripty/data
+echo "<p>Přijato: "$down"</p>" >> /home/student/Plocha/skripty/data
+echo "<p>Odesláno: "$up"</p>" >> /home/student/Plocha/skripty/data
+echo "<p>Volné místo na disku: "$disk"B</p>" >> /home/student/Plocha/skripty/data
+echo "<p>Právě je připojeno: "$users "uživatelů</p>" >> /home/student/Plocha/skripty/data
+echo "<code>Datum: "$cas"</code>" >> /home/student/Plocha/skripty/data
+echo "<br><br>" >> /home/student/Plocha/skripty/data
