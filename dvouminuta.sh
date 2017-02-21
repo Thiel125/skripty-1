f239d38f42ecbd4792b58a1ef9b403e96805627e
DanoCZE <admin@lukyn-gaming.eu>
2017-02-21 08:05:39 +0100

    Oprava bugu s názvem

--- dvouminuta.sh
+++ dvouminuta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cas=`date +"%T (%d.%m.%Y)"`
-echo "(Třiminutový skript) Aktuální čas je: "$cas >> /home/student/Plocha/skripty/cas.txt
+echo "(Dvouminutový skript) Aktuální čas je: "$cas >> /home/student/Plocha/skripty/cas.txt
---
+++ triminuta.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cas=`date +"%T (%d.%m.%Y)"`
+echo "(Třiminutový skript) Aktuální čas je: "$cas >> /home/student/Plocha/skripty/cas.txt
